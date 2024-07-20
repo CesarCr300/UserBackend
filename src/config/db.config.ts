a09d3ffb2827d4c72194f9318b55176c036c6b81
CesarCr300 <cesarjcontreras30@gmail.com>
2024-07-20 17:58:52 -0500

    deploy: allowing connection with TIDB

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,8 +1,10 @@
 import { TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
 import { env } from 'process';
+import { readFileSync } from 'fs';
 
 export const dbConfig: TypeOrmModuleAsyncOptions = {
   useFactory: () => {
+    console.log('env.TIDB_CA_PATH', env.TIDB_CA_PATH);
     return {
       type: 'mysql',
       host: env.DATABASE_HOST,
@@ -12,6 +14,13 @@ export const dbConfig: TypeOrmModuleAsyncOptions = {
       database: env.DATABASE_NAME,
       entities: [],
       autoLoadEntities: true,
+      ssl:
+        env.TIDB_ENABLE_SSL === 'true'
+          ? {
+              minVersion: 'TLSv1.2',
+              ca: env.TIDB_CA_PATH ? readFileSync(env.TIDB_CA_PATH) : null,
+            }
+          : null,
     };
   },
 };
